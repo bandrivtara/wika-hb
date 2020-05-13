414863b45f68f990f28a7f8c7d7bad3384b05018
Taras Bandrivskyy <bandrivtara@gmail.com>
2020-05-13 22:13:34 +0200

    Updates

---
+++ precache-manifest.55ac1215532b83d2a621e9578bb7395c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5add964fe6328a6e60c16688ccacae78",
+    "url": "/wika-hb/index.html"
+  },
+  {
+    "revision": "4eab62011ce7b31db6e5",
+    "url": "/wika-hb/static/css/main.d58ba327.chunk.css"
+  },
+  {
+    "revision": "ae9d1c54ed6644a4c9a3",
+    "url": "/wika-hb/static/js/2.f4150b10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wika-hb/static/js/2.f4150b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eab62011ce7b31db6e5",
+    "url": "/wika-hb/static/js/main.a65a7275.chunk.js"
+  },
+  {
+    "revision": "19e339e1df6c498ef116",
+    "url": "/wika-hb/static/js/runtime-main.8f2a3e52.js"
+  },
+  {
+    "revision": "b19bbf88ddcd15444411d87db1191c32",
+    "url": "/wika-hb/static/media/ballons.b19bbf88.png"
+  },
+  {
+    "revision": "5eaefafac1f880d121633e6246320634",
+    "url": "/wika-hb/static/media/hb-text.5eaefafa.png"
+  }
+]);
